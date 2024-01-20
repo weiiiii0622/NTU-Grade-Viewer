9f8f7b1f9d66ec84835ba0c39314c2c9cc312933
weiiiii0622 <weiiiii.0622@gmail.com>
2024-01-21 01:53:46 +0800

    update structure

--- app/parse_page.py
+++ app/parse_page.py
@@ -4,29 +4,7 @@ import re
 import bs4
 from bs4 import Tag
 
-
-GRADES = ("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "F")
-RE_SEMESTER = re.compile(r"\d+-\d+")
-
-
-@dataclass
-class Course:
-    id1: str  # 課號 e.g. CSIE1212
-    id2: str  # 課程識別碼 e.g. 902 10750
-    title: str
-
-
-@dataclass
-class GradeInfo:
-    course: Course
-    semester: str
-    grade: str
-    dist: tuple[float, float, float]  # lower, same, higher
-
-    def __postinit__(self):
-        assert sum(self.dist) - 100 < 1 and len(self.dist) == 3
-        assert self.grade in GRADES
-        assert RE_SEMESTER.match(self.semester)
+from module.course import Course, GradeInfo, GRADES
 
 
 def get_infos(row: Tag, classes: list[str]) -> list[str]:
---
+++ app/score_distribution.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+from module.course import Distrubution, GRADES
+
+# Constants
+print(GRADES)
+
