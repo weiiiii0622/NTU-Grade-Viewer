34c311818049625a0348dec3e08f7e06310a5408
kc0506 <kchong0506@gmail.com>
2024-03-22 00:18:02 +0800

    Update version to 1.0.2
    Update check-extension-pr action

--- release/get_updates.py
+++ release/get_updates.py
@@ -1,15 +1,26 @@
 import os
+import re
 import sys
 
 version = sys.argv[1]
 
-# version = "1.0.2"
 history = open("./release/HISTORY.md", encoding="utf-8").read()
 
-st = history.index("##")
-end = history[st + 1 :].index("##")
+st = history.index("## v")
+end = history[st + 1 :].index("## v")
 updates = history[st:end]
 assert updates.startswith(f"## v{version}"), "Version doesn't match!"
 
-with open(os.environ["GITHUB_OUTPUT"], "a", encoding="utf-8") as f:
+if os.getenv("MODE") == "TEXT":
+    updates_ln = updates.splitlines()
+    pattern = re.compile(r"(#+\s+)|(-\s+)|(\*\s+)")
+
+    # todo: add emoji?
+    def remove_syntax(ln: str):
+        return pattern.sub("", ln)
+
+    updates = "".join(map(remove_syntax, updates_ln))
+
+
+with open(os.getenv("GITHUB_OUTPUT", ""), "a", encoding="utf-8") as f:
     f.write(f"updates={ updates }")
